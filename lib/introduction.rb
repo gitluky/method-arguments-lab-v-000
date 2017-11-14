8c65dcbef6a6a9ca3feb8bc35c2ab9658ab2953a
Jonathan Luk <jluk87@gmail.com>
2017-11-14 01:24:08 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,5 +4,5 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  put "Hi, my name is Dan and I am learning to program in ${language}"
+  puts "Hi, my name is Dan and I am learning to program in ${language}"
 end
